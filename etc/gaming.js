58737ce957375dae14bef9275c1052e2bd2ab65c
christopher liu, of course! <github@christopherliu.net>
2011-10-24 14:27:36 -0400

    Updated dice functionality

--- etc/gaming.js
+++ etc/gaming.js
@@ -2,6 +2,7 @@ if (!Gaming)
     var Gaming = {};
 /**
 @class A single roll of the die.
+@version NewDice
 */
 Gaming.DiceRoll = function(sides) {
     var value;
@@ -21,17 +22,30 @@ Gaming.DiceRoll = function(sides) {
     this.getValue = function() {
         return value;
     };
-}
+};
 /**
-Returns an array of DiceRolls.
+Returns an array of DiceRolls, with an additional method getValue
 @example
-//To roll 1d6
-Gaming.RollDice(1,6)
+//To roll 2d6, reroll one die, and get the end result.
+var myDie = Gaming.RollDice(2,6);
+myDie[0].reroll();
+var mySpeedRoll = myDie.getValue();
+@version NewDice
 */
 Gaming.RollDice = function(numberOfDice, sides) {
     var diceRolls = [];
     for(var i = 0; i < numberOfDice; i++) {
         diceRolls.push(new Gaming.DiceRoll(sides));
     }
+    diceRolls.getValue = function() {
+        return sum($.map(diceRolls, function(roll) { return roll.getValue(); }));
+    };
     return diceRolls;
-}
+};
+Gaming.TestDice = function() {
+    var myDie = Gaming.RollDice(2,6);
+    myDie[0].reroll();
+    var mySpeedRoll = myDie.getValue();
+    alert(mySpeedRoll);
+};
+Gaming.TestDice();
