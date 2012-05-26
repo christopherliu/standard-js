760d6b005bcf47767a2047a5935f65eefe60c962
Christopher Liu <github@christopherliu.net>
2012-05-26 09:43:35 -0400

    Added PostToURL method

--- browser/DOMUtils.js
+++ browser/DOMUtils.js
@@ -14,15 +14,41 @@ if (!standard_library.DOMUtils) {
  * 
  */
 standard_library.DOMUtils.ExtractAttributes =
-		function _extractAttributes(element) {
-			"use strict";
+	function _extractAttributes(element) {
+		"use strict";
 
-			var attributes = {};
-			Array.prototype.filter.call(element.attributes,
-					function(attrPair) {
-						return attrPair.value !== "";
-					}).forEach(function(attrPair) {
-				attributes[attrPair.name] = attrPair.value;
-			});
-			return attributes;
-		};
+		var attributes = {};
+		Array.prototype.filter.call(element.attributes, function(attrPair) {
+			return attrPair.value !== "";
+		}).forEach(function(attrPair) {
+			attributes[attrPair.name] = attrPair.value;
+		});
+		return attributes;
+	};
+standard_library.DOMUtils.PostToURL = function(attributes, params) {
+	attributes.method = attributes.method || "post";
+
+	// The rest of this code assumes you are not using a library.
+	// It can be made less wordy if you use one.
+	var form = document.createElement("form");
+
+	for ( var key in attributes) {
+		if (attributes.hasOwnProperty(key)) {
+			form.setAttribute(key, attributes[key]);
+		}
+	}
+
+	for ( var key in params) {
+		if (params.hasOwnProperty(key)) {
+			var hiddenField = document.createElement("input");
+			hiddenField.setAttribute("type", "hidden");
+			hiddenField.setAttribute("name", key);
+			hiddenField.setAttribute("value", params[key]);
+
+			form.appendChild(hiddenField);
+		}
+	}
+
+	document.body.appendChild(form);
+	form.submit();
+};
