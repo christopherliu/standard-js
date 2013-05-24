79229933d2701a0a36feb0fcf09eb9f9e6b3dece
Christopher Liu <chris@adventurecow.com>
2013-05-24 17:48:40 -0400

    Attempting to exclude unused files from JSHint, stop heeding space/tab warnings

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,26 @@
-module.exports = function (grunt) {
-	//See: https://github.com/Krinkle/travis-ci-node-and-browser-qunit
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-qunit');
+module.exports = function(grunt) {
+    //See: https://github.com/Krinkle/travis-ci-node-and-browser-qunit
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
-	grunt.initConfig({
-		jshint: {
-			files: ['*.js', '*/*.js'],
-			options: {
-				smarttabs: true
-			}
-		},
-		qunit: {
-			files: ['tests/index.html']
-		}
-	});
+    grunt.initConfig({
+        jshint : {
+            files : ['*.js', '*/*.js'],
+            options : {
+                jshintignore: '.jshintignore',
+                smarttabs : true
+            },
+            ignore_warning : {
+                options : {
+                    '-W033' : true,
+                }
+            }
+        },
+        qunit : {
+            files : ['tests/index.html']
+        }
+    });
 
-	grunt.registerTask('test', ['jshint', 'qunit']);
-	grunt.registerTask('default', ['test']);
+    grunt.registerTask('test', ['jshint', 'qunit']);
+    grunt.registerTask('default', ['test']);
 };
